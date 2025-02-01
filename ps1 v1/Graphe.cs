93a447c38bdd50028ff6582dc9b254da35220fe7
vassCaR <jean.emile.vasseur@gmail.com>
2025-02-01 16:11:44 +0100

    classe graphe et matrice adjacence

--- ps1 v1/Graphe.cs
+++ ps1 v1/Graphe.cs
@@ -13,11 +13,13 @@ namespace ps1_v1
         // E est l'ensemble des liaisons/arêtes du graphe G
         private List<Noeud> v;
         private List<Lien> e;
+        bool[,] MatriceAdjacence;
 
         public Graphe (List<Noeud> noeuds, List<Lien> liens)
         {
             v = noeuds;
             e = liens ;
+            MatriceAdjacence = GenMatAdj(noeuds,liens); 
         }   
         public List<Noeud> V
         {
@@ -29,5 +31,50 @@ namespace ps1_v1
             get { return e; }
             set { e = value; }
         }
+
+        static bool[,] GenMatAdj(List<Noeud> noeuds, List<Lien> liens)
+        {
+            int n = noeuds.Count;
+            bool[,] mat = new bool[,] { };
+            for(int i = 0; i < n; i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    mat[i, j] = false;
+                    if (Lien_Existe(noeuds[i], noeuds[j]))
+                    {
+                        mat[i, j] = true; // la relation existe , on met dasn dans la case [i,j] de la matrice d'adjacence
+                    }
+                    
+                }
+            }
+            return mat;
+
+        }
+        public static bool Lien_Existe(Noeud n1, Noeud n2)
+        {
+            return n1.Liens.Any(l => l.Noeud1 == n2 || l.Noeud2 == n2);
+        }
+        public void AfficherMatriceAdjacence()
+        {
+            string chaine = "";
+            for(int i=0;i<v.Count;i++)
+            {
+                for(int j=0;j<=v.Count;j++)
+                {
+                    if (this.MatriceAdjacence[i,j]==true)
+                    {
+                        chaine += "1 ";
+                    }
+                    else
+                    {
+                        chaine += "0 ";
+                    }
+                }
+                chaine += "\n";
+            }
+            Console.WriteLine(chaine);
+        }
+
     }
 }
--- ps1 v1/Program.cs
+++ ps1 v1/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Création des nœuds
+            Noeud n1 = new Noeud(1);
+            Noeud n2 = new Noeud(2);
+            Noeud n3 = new Noeud(3);
+            List <Noeud> V = new List<Noeud> { n1, n2, n3 };
+            
+
+            // Création des liens
+            Lien l1 = new Lien(n1, n2,4);
+            Lien l2 = new Lien(n2, n3,2);
+            List<Lien> E = new List<Lien> { l1, l2 };
+
+            // Ajout des liens aux nœuds
+            n1.AjouterLien(l1);
+            n2.AjouterLien(l1);
+            n2.AjouterLien(l2);
+            n3.AjouterLien(l2);
+            Graphe graphe = new Graphe(V,E);
+            graphe.AfficherMatriceAdjacence();
         }
     }
 }
