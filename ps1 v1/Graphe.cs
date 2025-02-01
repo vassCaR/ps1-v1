65a4f96d078c25191f7d0eb805396613cc608aac
vassCaR <jean.emile.vasseur@gmail.com>
2025-02-01 15:20:44 +0100

    propriétés et constructeur Graphe

--- ps1 v1/Graphe.cs
+++ ps1 v1/Graphe.cs
@@ -8,5 +8,24 @@ namespace ps1_v1
 {
     internal class Graphe
     {
+        //graphe simple G(V,E)
+        private List<Noeud> v;
+        private List<Lien> e;
+
+        public Graphe (List<Noeud> noeuds, List<Lien> liens)
+        {
+            v = noeuds;
+            e = liens ;
+        }   
+        public List<Noeud> V
+        {
+            get { return v; }
+            set { v = value; }
+        }
+        public List<Lien> E
+        {
+            get { return e; }
+            set { e = value; }
+        }
     }
 }
